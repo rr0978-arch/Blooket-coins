c5f12429ded7cfd7039256f8ae5427836259940e
rr0978-arch <rr0978@tlascholar.com>
2025-09-19 13:08:32 -0500

    Update Coins.js

--- Coins.js
+++ Coins.js
@@ -22,13 +22,13 @@ async function addCurrencies() {
         },
         body: JSON.stringify({
             addedTokens: tokens,
-            addedXp: 300,
+            addedXp: 3000,
             name: await getName(myToken)
         })
     });
 
     if (response.status == 200) {
-        alert(`${tokens} tokens and 300 XP added to your account!`);
+        alert(`${tokens} tokens and 3000 XP added to your account!`);
     } else {
         alert('An error occured.');
     };
